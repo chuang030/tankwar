326a399fffd798b0f4d58d1387060e51e9773800
chi <chuang145836@gmail.com>
2020-12-19 14:24:05 +0800

    視窗測試OK&坦克測試OK

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,5 +1,26 @@
-public class GameClient {
-    public static void main(String[] args) {
-        System.out.println("t");
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent{
+    private int screenWidth;
+    private int screenHeight;
+
+    GameClient(){
+        this(800,600);
+    }
+
+    public GameClient(int screenWidth, int screenHeight) {
+        this.screenWidth = screenWidth;
+        this.screenWidth = screenHeight;
+
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.drawImage(new ImageIcon("assets/images/itankD.png").getImage(),
+                100,100,null);
     }
 }
---
+++ src/main/java/TankGame.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class TankGame {
+
+    public static void main(String[] args) {
+        GameClient gameClient = new GameClient(1024,600);
+        JFrame frame = new JFrame();
+        frame.add(gameClient);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                System.out.println((char)e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+        });
+    }
+}
